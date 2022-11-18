dc10d1bac43f0e62e5a5edb847a149f737218a3e
Jithinppp <jithinp968@gmail.com>
2022-11-18 22:35:12 +0530

    completed final cool peps v2 of monster-rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import CardList from "./components/card-list/Card-list.component";
+import SearchBox from "./components/search-box/Search-box.component";
 
-function App() {
+const App = () => {
+  // console.log("app render");
+  // defining states
+  const [searchField, setSearchField] = useState("");
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  // making sideEffect
+  useEffect(() => {
+    // console.log("fetching from server API");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  // onChange input handler
+  const onSearchChange = (e) => {
+    // console.log("change in input");
+    const searchField = e.target.value.toLowerCase();
+    setSearchField(() => {
+      return searchField;
+    });
+  };
+
+  // filtering the users
+  useEffect(() => {
+    console.log("filtering users");
+    const newFilteredUsers = users.filter((user) => {
+      return user.name.toLowerCase().includes(searchField);
+    });
+    setFilteredUsers(newFilteredUsers);
+  }, [users, searchField]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="main_title">Cool peps</h1>
+      <SearchBox
+        onSearchChange={onSearchChange}
+        placeHolder="search users..."
+        className="search_box"
+        type="search"
+      />
+      <CardList users={filteredUsers} />
     </div>
   );
-}
+};
+
+// !class based approach
+// import { Component } from "react";
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       users: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((res) => res.json())
+//       .then((data) =>
+//         this.setState((state) => {
+//           return { users: data };
+//         })
+//       );
+//   }
+
+//   onSearchChange = (e) => {
+//     const searchField = e.target.value.toLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+
+//   render() {
+//
+//     return (
+//       <div className="App">
+//         <h1 className="main_title">Cool peps</h1>
+//         <SearchBox
+//           onSearchChange={onSearchChange}
+//           placeHolder="search users..."
+//           className="search_box"
+//           type="search"
+//         />
+//         <CardList users={filteredUsers} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/card/Card.component.jsx
@@ -0,0 +1,41 @@
+import "./card.style.css";
+
+const Card = ({ user }) => {
+  const { name, website } = user;
+  return (
+    <div className="card_container">
+      <h1 className="card_title">{name}</h1>
+      <p className="card_subtitle">{website}</p>
+      <div className="avatar_container">
+        <img
+          className="avatar"
+          src={`https://avatars.dicebear.com/api/adventurer/:${name}.svg`}
+          alt="user avatar"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+// import { Component } from "react";
+// class Card extends Component {
+//   render() {
+//     const { name, website } = this.props.user;
+//     return (
+//       <div className="card_container">
+//         <h1 className="card_title">{name}</h1>
+//         <p className="card_subtitle">{website}</p>
+//         <div className="avatar_container">
+//           {/* https://avatars.dicebear.com/api/micah/:aj.svg */}
+//           <img
+//             className="avatar"
+//             src={`https://avatars.dicebear.com/api/adventurer/:${name}.svg`}
+//             alt="user avatar"
+//           ></img>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+export default Card;
